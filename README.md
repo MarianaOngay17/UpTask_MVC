2772ab9fd96413fe2e33d12d866eb542427df973
Mariana <marianaongay.01@gmail.com>
2025-09-11 13:52:47 -0600

    Router y vista login

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # UpTask_MVC
 Proyecto Curso Desarrollo Web
+
+Pasos iniciales:
+
+Instalar dependencias
+
+    npm install
+
+Actualizar composer
+
+    composer update
+
---
+++ controllers/LoginController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class LoginController {
+
+    public static function login(Router $router){
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        }
+
+        $router->render('auth/login', [
+            'titulo' => 'Iniciar Sesión'
+        ]);
+
+    }
+
+    public static function logout(){
+        echo "desde logout";
+    }
+
+    public static function crear(Router $router){
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        }
+
+         $router->render('auth/crear', [
+            'titulo' => 'Crea tu cuenta en UpTask'
+        ]);
+
+    }
+
+    public static function olvide(){
+        echo "desde olvide";
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        }
+
+    }
+
+    public static function reestablecer(){
+        echo "desde reestablecer";
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        }
+
+    }
+    public static function mensaje(){
+        echo "desde mensaje";
+
+
+    }
+    public static function confirmar(){
+        echo "desde confirmar";
+
+    }
+}
