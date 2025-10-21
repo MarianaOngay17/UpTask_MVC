9cb377ca3fe35b192ac91b6f60660ade6a16bb6e
Mariana <marianaongay.01@gmail.com>
2025-10-21 12:34:59 -0600

    Añadir tareas modal

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -9,9 +9,42 @@
         modal.innerHTML = `
             <form class="formulario nueva-tarea">
                 <legend>Añade una nueva tarea</legend>
-            
+                <div class="campo">
+                    <label>Tarea</label>
+                    <input
+                        type="text"
+                        name="tarea"
+                        placeholder="Añadir Tarea al Proyecto Actual"
+                        id="tarea"
+                    />
+                </div>
+                <div class="opciones">
+                    <input type="submit" class="submit-nueva-tarea" value="Añadir Tarea" />
+                    <button type="button" class="cerrar-modal">Cancelar</button>
+                </div>
             </form>
         `;
+
+        setTimeout(() => {
+            const formulario = document.querySelector('.formulario');
+            formulario.classList.add('animar');
+        }, 0);
+
+        modal.addEventListener('click', function(e){
+            e.preventDefault();
+
+            if(e.target.classList.contains('cerrar-modal')){
+
+                const formulario = document.querySelector('.formulario');
+                formulario.classList.add('cerrar');
+                
+                setTimeout(() => {
+                    modal.remove();
+                }, 500);
+            }
+        });
+
+        document.querySelector('body').appendChild(modal);
     
     
     }
